b042f4b7e1399b91d522b0dc336f60be908e710b
Alex Palma <alex-palma@outlook.com>
2018-04-17 23:30:27 +1000

    test_rday.py: Improve fortnight_falls test for alternate case

--- test_rday.py
+++ test_rday.py
@@ -10,8 +10,12 @@ class rday_test_case(unittest.TestCase):
     def test_fortnight_falls(self):
       date1 = datetime.date(2018,01,07)
       date2 = datetime.date(2018,01,14)
+      date3 = datetime.date(2018,01,21)
       self.assertFalse(
         fortnight_falls(date1, date2)
+      )
+      self.assertTrue(
+        fortnight_falls(date1, date3)
       )
 
     def test_weekly_falls(self):
