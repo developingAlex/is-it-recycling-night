52d21d2da6db074d650734c81410ca0221b8be85
Alex Palma <alex-palma@outlook.com>
2018-04-17 23:27:17 +1000

    test_rday.py: Add initial unit tests
    starting off with simple unit tests for the two
    functions created so far:
    - fortnight_falls
    - weekly_falls

---
+++ test_rday.py
@@ -0,0 +1,23 @@
+#run by going `python -m unittest test_rday`
+
+import unittest
+import datetime
+from rday import fortnight_falls
+from rday import weekly_falls
+
+class rday_test_case(unittest.TestCase):
+
+    def test_fortnight_falls(self):
+      date1 = datetime.date(2018,01,07)
+      date2 = datetime.date(2018,01,14)
+      self.assertFalse(
+        fortnight_falls(date1, date2)
+      )
+
+    def test_weekly_falls(self):
+      date1 = datetime.date(2018,01,07)
+      date2 = datetime.date(2018,01,14)
+      self.assertTrue(
+        weekly_falls(date1, date2)
+      )
+
